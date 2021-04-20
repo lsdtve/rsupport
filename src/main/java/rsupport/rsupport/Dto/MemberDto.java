c7d15663f361b3f882f26ee119c91b735b02b44e
배용균 <lsdtve@gmail.com>
2021-04-20 13:58:07 +0900

    Add Member Constructor

--- src/main/java/rsupport/rsupport/Dto/MemberDto.java
+++ src/main/java/rsupport/rsupport/Dto/MemberDto.java
@@ -13,4 +13,13 @@ public class MemberDto {
     private String teamName;
     private String grade;
     private String position;
+
+    public MemberDto(String name, String number, String phone, String teamName, String grade, String position) {
+        this.name = name;
+        this.number = number;
+        this.phone = phone;
+        this.teamName = teamName;
+        this.grade = grade;
+        this.position = position;
+    }
 }
